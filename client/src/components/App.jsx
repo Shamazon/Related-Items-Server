a1d8ded308a10de7b335d03c47abfb3e5b142c75
Zachary Hill <zach@zacharyhill.xyz>
2018-04-13 11:24:53 -0700

    Display all related products

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import RelatedProducts from './RelatedProducts.jsx';
 
 import './styles/App.css';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    const productId = this.props.id || 0;
+    this.setRelatedProducts(productId);
+  }
+
+  componentWillReceiveProps() {
+    this.setRelatedProducts(props.id);
+  }
+
+  setRelatedProducts(productId) {
+    axios.get(`http://localhost:9001/products/${productId}/related`)
+      .then((data) => {
+        const products = data.data;
+        this.setState({ products });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+
   render() {
     return (
-      <div>
-        Hello!
-      </div>
-    )
+      <RelatedProducts 
+        products={this.state.products}
+      />
+    );
   }
 }
 
