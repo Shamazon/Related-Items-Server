c3096e2d66e73fd7abd2628d856216b3c773ffaf
Zachary Hill <zach@zacharyhill.xyz>
2018-04-13 16:15:18 -0700

    Implement some style and functionality

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -6,16 +6,37 @@ import Product from './Product.jsx';
 import styles from './styles/Products.css';
 
 export default (props) => {
+
+  const productsClassName = 'Products__inner___2mjZo';
+  const goLeftClassName = 'Products__goLeft___3ROMB';
+
+  const scrollRight = (event) => {
+    document.getElementsByClassName(productsClassName)[0].scrollLeft += 500;
+    // document.getElementsByClassName(goLeftClassName)[0].style.display = 'initial';
+    event.currentTarget.style = 'right: 0';
+  };
+
+  const scrollLeft = (event) => {
+    document.getElementsByClassName(productsClassName)[0].scrollLeft -= 500;
+    // document.getElementsByClassName(goLeftClassName)[0].style.display = 'initial';
+    event.currentTarget.style = 'right: 0';
+  };
+
   if (props.products.length) {
     return (<div className={styles.relatedProducts}>
+      
+      
+      <div className={styles.inner}>
+        {props.products.map((product) => (
+          <Product
+            product={product}
+            key={product.id}
+          />
+        ))}
+      </div>
 
-      {props.products.map((product) => (
-        <Product
-          product={product}
-          key={product.id}
-        />
-      ))}
-
+      <button className={styles.goLeft} onClick={scrollLeft}>&lt;</button>
+      <button className={styles.goRight} onClick={scrollRight}>&gt;</button>
     </div>);
   } else {
     return <span>Loading...</span>
