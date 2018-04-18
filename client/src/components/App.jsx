2737112558440238498b0f1a2524737af4cf973a
Zachary Hill <zach@zacharyhill.xyz>
2018-04-18 15:08:57 -0700

    Fix bug in componenetWillReceieveProps

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -21,8 +21,8 @@ export default class App extends React.Component {
     this.setRelatedProducts(productId);
   }
 
-  componentWillReceiveProps() {
-    this.setRelatedProducts(this.props.id);
+  componentWillReceiveProps(nextProps) {
+    this.setRelatedProducts(nextProps.id);
   }
 
   setRelatedProducts(productId) {
