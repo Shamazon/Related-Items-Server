09d2c64e66e24dd0fdcb7f874070ba6fad12b493
Zachary Hill <zach@zacharyhill.xyz>
2018-04-24 12:26:09 -0700

    Get overall test coverage over 60%

--- __tests__/RelatedProducts.test.js
+++ __tests__/RelatedProducts.test.js
@@ -4,6 +4,59 @@ import App from '../client/src/components/App.jsx';
 import RelatedProducts from '../client/src/components/RelatedProducts.jsx';
 
 describe('<RelatedProducts />', () => {
+
+  const products = [{
+    id: 10,
+    name: 'test product',
+    price: '10.00',
+    imageUrls: ['fakeURL']
+  }];
+
+  const moreProducts = [
+    {
+      id: 10,
+      name: 'test product',
+      price: '10.00',
+      imageUrls: ['fakeURL']
+    },
+    {
+      id: 11,
+      name: 'test product 2',
+      price: '25.00',
+      imageUrls: ['fakeURL2']
+    },
+    {
+      id: 12,
+      name: 'test product 3',
+      price: '50.00',
+      imageUrls: ['fakeURL3']
+    },
+    {
+      id: 13,
+      name: 'test product 4',
+      price: '100.00',
+      imageUrls: ['fakeURL4']
+    },
+    {
+      id: 14,
+      name: 'test product 5',
+      price: '125.00',
+      imageUrls: ['fakeURL5']
+    },
+    {
+      id: 15,
+      name: 'test product 6',
+      price: '150.00',
+      imageUrls: ['fakeURL6']
+    },
+    {
+      id: 16,
+      name: 'test product 7',
+      price: '175.00',
+      imageUrls: ['fakeURL7']
+    },
+];
+
   it('should call setRelatedProducts when component mounts', () => {
     const spy = jest.spyOn(App.prototype, 'setRelatedProducts');
     const component = enzyme.mount(<App />);
@@ -17,12 +70,6 @@ describe('<RelatedProducts />', () => {
   });
 
   it('should render products when props are received', () => {
-    const products = [{
-      id: 10,
-      name: 'test product',
-      price: '10.00',
-      imageUrls: ['fakeURL']
-    }];
     const wrapper = enzyme.shallow(
       <RelatedProducts
         products={products}
@@ -30,4 +77,10 @@ describe('<RelatedProducts />', () => {
     );
     expect(wrapper.find('div').length).toBeGreaterThan(0);
   });
+
+  it('should render buttons to scroll products', () => {
+    const wrapper = enzyme.mount(<RelatedProducts products={moreProducts} />);
+    expect(wrapper.find('#leftButton')).toHaveLength(1);
+    expect(wrapper.find('#rightButton')).toHaveLength(1);
+  });  
 });
