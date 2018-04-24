8d526b8e09783b0352a79d1ff77c383936b6868c
Zachary Hill <zach@zacharyhill.xyz>
2018-04-23 18:56:02 -0700

    Add jest setup file so each test does not have to configure the enzyme adapter

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import App from '../client/src/components/App.jsx';
-
-enzyme.configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders 1 <App /> component', () => {
