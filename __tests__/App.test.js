7a22aa93a860a1e4d5ded1e79bc4028407f30703
Zachary Hill <zach@zacharyhill.xyz>
2018-04-23 19:59:40 -0700

    Implement test for setRelatedProducts

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -3,9 +3,17 @@ import enzyme from 'enzyme';
 import App from '../client/src/components/App.jsx';
 
 describe('<App />', () => {
-  it('renders 1 <App /> component', () => {
+  it('should render one <App /> component', () => {
     const component = enzyme.shallow(<App />);
     expect(component).toHaveLength(1);
   });
+
+  it('should call setRelatedProducts when component mounts', () => {
+    const spy = jest.spyOn(App.prototype, 'setRelatedProducts');
+    const component = enzyme.mount(<App />);
+    component.update();
+    expect(spy).toHaveBeenCalled();
+  });
+
 });
 
